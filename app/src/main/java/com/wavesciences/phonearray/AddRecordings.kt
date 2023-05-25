13b662a8f65a96ded3b6c84bcf50dbcba9b2b8f7
alexa padberg <alexa.padberg@wavesciencescorp.com>
2023-05-25 10:41:10 -0400

    created a recycler view to display recordings on the ManageRecordings.kt as a button.

--- app/src/main/java/com/wavesciences/phonearray/AddRecordings.kt
+++ app/src/main/java/com/wavesciences/phonearray/AddRecordings.kt
@@ -1,15 +1,21 @@
 package com.wavesciences.phonearray
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.media.AudioFormat
+import android.media.AudioManager.AudioPlaybackCallback
+import android.media.AudioPlaybackConfiguration
 import android.media.AudioRecord
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 import androidx.activity.ComponentActivity
 
 import com.wavesciences.phonearray.databinding.ActivityAddRecordingsBinding
+import com.wavesciences.phonearray.databinding.ActivityManageRecordingsBinding
 
 import java.io.FileOutputStream
 
@@ -18,9 +24,13 @@ class AddRecordings : ComponentActivity() {
     private var recorder1: AudioRecord? = null
     private var recorder2: AudioRecord? = null
     private var isRecording = false
-    private val recordName = binding?.recordingName.toString()
+    private lateinit var  recordName: String
+
     private var recordingThread1: Thread? = null
     private var recordingThread2: Thread? = null
+
+    private lateinit var mediaPlayer: MediaPlayer
+
     private val pcmBufferSize: Int
         get() {
             val pcmBufSize = AudioRecord.getMinBufferSize(
@@ -40,11 +50,66 @@ class AddRecordings : ComponentActivity() {
         }
         binding!!.stopRecordingBtn.setOnClickListener{
             stopRecording()
+            val recordName = binding?.recordingName?.text.toString()
+            val fileName1 = getExternalFilesDir(null).toString() + "/"+ recordName +"_record1.1.pcm"
+            val fileName2 = getExternalFilesDir(null).toString() + "/" + recordName + "_record2.1.pcm"
+
+            val intent = Intent(applicationContext,
+                ManageRecordings::class.java)
+            intent.putExtra("recpath1", fileName1)
+            intent.putExtra("recpath2", fileName2)
+            startActivity(intent)
+
+
+            val toast = Toast.makeText(this, "$recordName saved to your recordings.", Toast.LENGTH_SHORT)
+            toast.show()
+            //TODO: add a toast saying that the recording was saved with its file name,
+            //TODO: when stop button is pressed we should also pass the recordings through with intents
+        }
+
+        binding!!.buttonTestRecordings.setOnClickListener{
+           try {
+                var mp = MediaPlayer()
+                mp.setDataSource(getExternalFilesDir(null).toString() + "/" + recordName + "_record1.1.pcm")
+                mp.start()
+                Log.d(TAG, "playing recording")
+            }
+           catch(e: Exception) {
+                Log.e(TAG, " Error playing recording", e)
+            }
+        }
+
+    }
+
+    private fun threadRecordings(recordingName: String, recorder: AudioRecord?, fileName: String): Thread {
+        return Thread {
+            val data = ByteArray(pcmBufferSize)
+            Log.d(TAG, ">>>>>>>>>>> STARTING THREAD: $recordingName")
+            try {
+                recorder?.let { validRecorder ->
+                    FileOutputStream(fileName).use { os ->
+                        var totalBytes: Long = 0
+                        while (isRecording) {
+                            val bytesRead = validRecorder.read(data, 0, data.size)
+                            if (bytesRead > 0) {
+                                totalBytes += bytesRead.toLong()
+                                os.write(data, 0, bytesRead)
+                            }
+                        }
+                        os.flush()
+                        Log.d(TAG, ">>>>>>>>>>> Total Bytes for $recordingName: $totalBytes")
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "[$recordingName] Error saving recording", e)
+            }
         }
     }
 
     @SuppressLint("MissingPermission")
     private fun startRecording() {
+        val recordName = binding?.recordingName?.text.toString()
+
         recorder1 = AudioRecord(
             MediaRecorder.AudioSource.MIC,
             SAMPLING_RATE_IN_HZ,
@@ -64,67 +129,16 @@ class AddRecordings : ComponentActivity() {
         isRecording = true
 
 
-        //Start the recording threads.
-        //TODO: Clean this up by encapsulating the thread method. Fine tune the buffer size.
-        recordingThread1 = Thread {
-            val data = ByteArray(pcmBufferSize)
+        val fileName1 = getExternalFilesDir(null).toString() + "/"+ recordName +"_record1.1.pcm"
+        val fileName2 = getExternalFilesDir(null).toString() + "/" + recordName + "_record2.1.pcm"
 
-            Log.d(TAG, ">>>>>>>>>>> STARTING THREAD 1")
-            try {
-                FileOutputStream(
-                    getExternalFilesDir(null)
-                        .toString() + "/"+ recordName +"_record1.1.pcm"
-                ).use { os ->
-                    var totalBytes: Long = 0
-                    while (isRecording) {
-                        val bytesRead = recorder1!!.read(data, 0, data.size)
-                        if (bytesRead > 0) {
-                            totalBytes += bytesRead.toLong()
-                            os.write(data, 0, bytesRead)
-                        }
-                    }
-                    os.flush()
-                    Log.d(TAG,">>>>>>>>>>> Total Bytes 1: $totalBytes")
+        recordingThread1 = threadRecordings("recording1", recorder1!!, fileName1)
+        recordingThread2 = threadRecordings("recording2", recorder2!!, fileName2)
 
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "[recordingThread1] Error saving recording", e)
-            }
-        }
-
-        recordingThread2 = Thread {
-            val data = ByteArray(pcmBufferSize)
-
-            //val recordName = binding?.recordingName.toString()
-
-            Log.d(TAG, ">>>>>>>>>>> STARTING THREAD 2")
-            try {
-                FileOutputStream(
-                    getExternalFilesDir(null)
-                        .toString() + "/" + recordName + "_record1.1.pcm"
-                ).use { os ->
-                    var totalBytes: Long = 0
-                    while (isRecording) {
-                        val bytesRead =recorder1!!.read(data, 0, data.size)
-                        if (bytesRead > 0) {
-                            totalBytes += bytesRead.toLong()
-                            os.write(data, 0, bytesRead)
-                        }
-                    }
-                    os.flush()
-                    Log.d(TAG,">>>>>>>>>>> Total Bytes 2: $totalBytes")
-
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "[recordingThread2] Error saving recording", e)
-            }
-
-        }
-
-        Log.d(TAG, "[buttonTestRecording] Starting the Recording")
         recordingThread1!!.start()
         recordingThread2!!.start()
     }
+
 
     private fun stopRecording() {
         Log.d(TAG, "[buttonTestRecording] Stopping the Recording")
@@ -137,6 +151,7 @@ class AddRecordings : ComponentActivity() {
         recorder2 = null  //Release for GC
     }
 
+
     companion object {
         val TAG = AddRecordings::class.java.simpleName
         private const val SAMPLING_RATE_IN_HZ = 44100
@@ -144,3 +159,5 @@ class AddRecordings : ComponentActivity() {
         private const val AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT
     }
 }
+
+
--- app/src/main/java/com/wavesciences/phonearray/ManageRecordings.kt
+++ app/src/main/java/com/wavesciences/phonearray/ManageRecordings.kt
@@ -1,16 +1,22 @@
 package com.wavesciences.phonearray
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.wavesciences.phonearray.databinding.ActivityManageRecordingsBinding
+import java.io.IOException
 
 
 class ManageRecordings: ComponentActivity() {
     private lateinit var binding: ActivityManageRecordingsBinding
+    private lateinit var mediaPlayer1: MediaPlayer
+    private lateinit var mediaPlayer2: MediaPlayer
+    private lateinit var recordingListAdapter: AdapterRecyclerView
+    private var recordingFilePaths: List<String> = emptyList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +24,52 @@ class ManageRecordings: ComponentActivity() {
         binding = ActivityManageRecordingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val rec1 = intent.getStringExtra("recpath1")
+        val rec2 = intent.getStringExtra("recpath2")
+        Log.d(TAG, "rec1: $rec1")
+        Log.d(TAG, "rec2: $rec2")
+
+        mediaPlayer1 = MediaPlayer()
+        mediaPlayer2 = MediaPlayer()
+        recordingFilePaths = listOfNotNull(rec1,rec2)
+
+        //Log.d(TAG, "recordingFilePaths: $recordingFilePaths")
+
+        println(recordingFilePaths.joinToString())
+        recordingListAdapter = AdapterRecyclerView(recordingFilePaths)
+        binding.recordingsHolder.adapter = recordingListAdapter
+        binding.recordingsHolder.layoutManager = LinearLayoutManager(this@ManageRecordings)
+
+
+
+//        try {
+//            mediaPlayer1.setDataSource(rec1)
+//            mediaPlayer2.setDataSource(rec2)
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//
+//        // Prepare media players
+//        mediaPlayer1.prepareAsync()
+//        mediaPlayer2.prepareAsync()
+//
+//
+//
+//        mediaPlayer1.setOnPreparedListener {
+//            mediaPlayer1.start()
+//        }
+//        mediaPlayer2.setOnPreparedListener {
+//            mediaPlayer2.start()
+//        }
+//
+
         binding.homeBtn.setOnClickListener {
             finish()
         }
 
+    }
+    companion object {
+        private const val TAG = "ManageRecordings"
     }
 }
 
